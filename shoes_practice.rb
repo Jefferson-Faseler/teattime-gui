22b055733064f09c9f7ba9fbe7b5d603df6df39b
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-19 21:37:27 -0500

    Add working timer method.

--- shoes_practice.rb
+++ shoes_practice.rb
@@ -1,20 +1,24 @@
 Shoes.app do
   button "Start Work" do
     @start_time = Time.now
-    @alert_time = Time.now + 1500
-
-    @work_label.replace "Enjoy your tea, see you at #{@alert_time.strftime("%I:%M%p")}"
-    sleep(5)
-    return `osascript -e 'display notification "Drink up old chap" with title "Tea Time!"'`
+    @alert_time = @start_time + 1500
+    @counter = para ""
+    animate(1) do |second|
+      if second >= 1500
+        return `osascript -e 'display notification "Drink up old chap" with title "Tea Time!"'`
+      else
+        @counter.replace "#{(second / 60).to_s.rjust(2,'0')}:#{(second % 60).to_s.rjust(2,'0')}"
+      end
+    end
   end
 
   button "Start Break" do
     @start_time = Time.now
-    @alert_time = Time.now + 1500
+    @alert_time = Time.now + 300
 
-    @work_label.replace "Enjoy your tea, see you at #{@alert_time.strftime("%I:%M%p")}"
+    @work_label.replace "Time to work again at #{@alert_time.strftime("%I:%M%p")}"
     sleep(5)
-    return `osascript -e 'display notification "Drink up old chap" with title "Tea Time!"'`
+    return `osascript -e 'display notification "Back to the grind" with title "One more sip!"'`
   end
 
 
