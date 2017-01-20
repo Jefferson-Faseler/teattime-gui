f72ae21075f6dc9b57fec77505de1043fa10d493
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-20 13:10:26 -0500

    Working product.

--- shoes_practice.rb
+++ shoes_practice.rb
@@ -1,52 +1,58 @@
 Shoes.app do
 
+#styling for program
   background "#FFDBAA".."#805215"
   @image = image "static/tea-icon-14700.png"
   @image.move(380,390)
-  @label = banner "Time to work"
-  @label.move(10,420)
-  @button_title = 'Start Work'
-  @length = 1500
+  @work_label = banner "Time to work"
+  @break_label = banner "Time for tea"
+  @work_label.move(10,420)
+  @break_label.move(10,420)
+  @break_label.hide
+  @work_title = 'Start Work'
+  @break_title = 'Start Break'
   @counter = banner "     "
   @counter.move(380, 300)
 
-  #inversed script because of order of events
-  @osascript = `osascript -e 'display notification "Back to the grind" with title "One more sip!" sound name "Glass"'`
-
-  @button = button @button_title do
-    @start_time = Time.now
-    @alert_time = @start_time + @length
+#business logic
+  @work_button = button @work_title do
+    @break_button.hide
+    @break_label.hide
+    @work_start = Time.now
     @timer = animate(1) do |second|
-      if second >= @length
-        transform_page
-        return @osascript
+      if second > 1500
+        @work_label.hide
+        @break_button.show
+        @break_label.show
+        return `osascript -e 'display notification "Drink up old chap" with title "Tea Time!" sound name "Glass"'`
       else
         @counter.replace "#{(second / 60).to_s.rjust(2,'0')}:#{(second % 60).to_s.rjust(2,'0')}"
       end
     end
   end
 
-  @button.move(10, 380)
+  @break_button = button @break_title do
+    @work_button.hide
+    @work_label.hide
+    @break_start = Time.now
+    @timer = animate(1) do |second|
+      if second > 300
+        @break_label.hide
+        @work_button.show
+        @work_label.show
+        return `osascript -e 'display notification "Back to the grind" with title "One more sip!" sound name "Glass"'`
+      else
+        @counter.replace "#{(second / 60).to_s.rjust(2,'0')}:#{(second % 60).to_s.rjust(2,'0')}"
+      end
+    end
+  end
 
-  @pause = button 'Pause Timer' do
+  @stop = button 'Stop Timer' do
     @timer.toggle
   end
 
-  @pause.move(10,350)
-
-  def transform_page
-    if @button_title == 'Start Work'
-      @button_title = 'Start Break'
-      @length = 300
-      @osascript = `osascript -e 'display notification "Drink up old chap" with title "Tea Time!" sound name "Glass"'`
-      @label = banner "Time for tea"
-    else
-      @button_title = 'Start Work'
-      @length = 1500
-      @osascript = `osascript -e 'display notification "Back to the grind" with title "One more sip!" sound name "Glass"'`
-      @label = banner "Time to work"
-    end
-    @counter.clear
-  end
+  @break_button.move(10, 320)
+  @stop.move(10,350)
+  @work_button.move(10, 380)
 
 end
