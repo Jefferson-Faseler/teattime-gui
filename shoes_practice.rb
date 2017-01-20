91cf414c27a09f02e8d184161557f3af693bc18f
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-19 22:46:27 -0500

    Add styling.

--- shoes_practice.rb
+++ shoes_practice.rb
@@ -1,18 +1,23 @@
 Shoes.app do
 
+  @image = image "static/tea-icon-14700.png"
+  @image.move(380,390)
+  @label = banner "Time to work"
+  @label.move(10,420)
   @button_title = 'Start Work'
   @length = 1500
+  @counter = banner "     "
+  @counter.move(380, 300)
+
   #inversed script because of order of events
   @osascript = `osascript -e 'display notification "Back to the grind" with title "One more sip!"'`
 
-  button @button_title do
+  @button = button @button_title do
     @start_time = Time.now
     @alert_time = @start_time + @length
-    @counter = para ""
     animate(1) do |second|
       if second >= @length
         transform_page
-        @counter.clear
         return @osascript
       else
         @counter.replace "#{(second / 60).to_s.rjust(2,'0')}:#{(second % 60).to_s.rjust(2,'0')}"
@@ -20,12 +25,14 @@ Shoes.app do
     end
   end
 
+  @button.move(10, 380)
+
   def transform_page
     @button_title = 'Start Break'
     @length = 300
+    @counter.clear
     @osascript = `osascript -e 'display notification "Drink up old chap" with title "Tea Time!"'`
+    @label = banner "Time for tea"
   end
 
-  @work_label = para "Press ", strong("start work"), " to work towards your next cup."
-  @break_label = para "Or press ", strong("start break"), " to enjoy your tea."
 end
