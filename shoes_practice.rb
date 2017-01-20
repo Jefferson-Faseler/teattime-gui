6de0b579b119fb5fd95019b50688e201072e89c9
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-19 21:47:19 -0500

    Refactor code. Working logic.

--- shoes_practice.rb
+++ shoes_practice.rb
@@ -1,26 +1,30 @@
 Shoes.app do
-  button "Start Work" do
+
+  @button_title = 'Start Work'
+  @length = 1500
+  #inversed script because of order of events
+  @osascript = `osascript -e 'display notification "Back to the grind" with title "One more sip!"'`
+
+  button @button_title do
     @start_time = Time.now
-    @alert_time = @start_time + 1500
+    @alert_time = @start_time + @length
     @counter = para ""
     animate(1) do |second|
-      if second >= 1500
-        return `osascript -e 'display notification "Drink up old chap" with title "Tea Time!"'`
+      if second >= @length
+        def transform_page
+        @counter.clear
+        return @osascript
       else
         @counter.replace "#{(second / 60).to_s.rjust(2,'0')}:#{(second % 60).to_s.rjust(2,'0')}"
       end
     end
   end
 
-  button "Start Break" do
-    @start_time = Time.now
-    @alert_time = Time.now + 300
-
-    @work_label.replace "Time to work again at #{@alert_time.strftime("%I:%M%p")}"
-    sleep(5)
-    return `osascript -e 'display notification "Back to the grind" with title "One more sip!"'`
+  def transform_page
+    @button_title = 'Start Break'
+    @length = 300
+    @osascript = `osascript -e 'display notification "Drink up old chap" with title "Tea Time!"'`
   end
-
 
   @work_label = para "Press ", strong("start work"), " to work towards your next cup."
   @break_label = para "Or press ", strong("start break"), " to enjoy your tea."
